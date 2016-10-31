45ec8abf223bc490b76f928f486c456671d55de0
Walter Varela <sen0rxol0@gmail.com>
2016-10-31 14:31:55 +0100

    Changes made to header, added placeholder, and initial scripts for functionality

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,1 +1,33 @@
-console.log("document ready!");
+(function() {
+    "use strict";
+
+    function init() {
+        console.log("document ready!");
+    }
+
+
+    function navHandler() {
+        var hamBtn = document.getElementById("ham--button");
+        var nav = document.getElementById("main--nav");
+
+        hamBtn.addEventListener("click", function(ev) {
+            ev.preventDefault();
+
+           if (!nav.classList.contains("nav--shown")) {
+                nav.classList.add("nav--shown");
+           } else {
+               nav.classList.remove("nav--shown");
+           }
+        });
+    } 
+
+
+    document.onreadystatechange = function() {
+        if (document.readyState === "complete") {
+            init();
+            navHandler();
+        }
+    }
+
+}());
+
